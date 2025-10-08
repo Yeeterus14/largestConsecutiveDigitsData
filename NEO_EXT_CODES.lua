bcddff64ca8e6d0e7a84df4b3b8e412da67b7dd8
Yeeterus14 <yeetusbiggereetus@gmail.com>
2025-10-08 17:33:58 +0200

    Create NEO_EXT_CODES.lua

---
+++ NEO_EXT_CODES.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		["Name"] = "dookie",
+		["Function"] = function(self)
+			return "swag atlanta family dolar store"
+		end,
+		["Ro-Powers"] = {
+			"4x Roaming Chance",
+			"16x Shiny Chance"
+		}
+	},
+}
