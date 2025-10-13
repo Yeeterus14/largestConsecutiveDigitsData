296aab4437534fa20681b792a58891a241db0f52
Yeeterus14 <yeetusbiggereetus@gmail.com>
2025-10-13 06:11:54 +0200

    Update NEO_EXT_CODES.lua

--- NEO_EXT_CODES.lua
+++ NEO_EXT_CODES.lua
@@ -26,4 +26,19 @@ return {
 			end
 		end
 	},
+  	{
+		["Name"] = "InviteEvent",
+		['Limit'] = false,
+		["Function"] = function(self)
+			if self.badges[3] then
+				self:addBagItems{id = 'bottlecap', quantity = 1}
+				self:addBagItems{id = 'machobrace', quantity = 1}
+				self:addBagItems{id = 'everstone', quantity = 1}
+				self:addBagItems{id = 'amuletcoin', quantity = 1}
+				return "Code Successfully Redeemed!"
+			else
+				return "You must have the 3rd Badge before redeeming this code.", true
+			end
+		end
+	},
 }
