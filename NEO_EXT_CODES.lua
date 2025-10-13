d4f09ae0b6d5a77ded4ee1807e448296bcdbaf6c
Yeeterus14 <yeetusbiggereetus@gmail.com>
2025-10-13 02:55:20 +0200

    Update NEO_EXT_CODES.lua

--- NEO_EXT_CODES.lua
+++ NEO_EXT_CODES.lua
@@ -1,7 +1,6 @@
 return {
   	{
 		["Name"] = "HawluchaMegaZA",
-		['Limit'] = 200,
 		["Function"] = function(self)
 			self:PC_sendToStore(self:newPokemon({
 				num = 701,
@@ -11,6 +10,23 @@ return {
 				untradable = true
 			}))
 			return "Code Successfully Redeemed!"
+		end
+	},
+	
+  	{
+		["Name"] = "Preboost",
+		['Date'] = 0,
+		['Limit'] = false,
+		["Function"] = function(self)
+			if self.badges[3] then
+				self:addBagItems{id = 'hawluchite', quantity = 1}
+				self:addBP(50)
+				self:addMoney(15000)
+				return "Code Successfully Redeemed!"
+				local PlayerData = _f.PlayerDataService[self.player];
+			else
+				return "You must have the 3rd Badge before redeeming this code.", true
+			end
 		end
 	},
 }
