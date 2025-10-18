2480e64910b101156f558b8c02f7fc61b79ce1a1
Yeeterus14 <yeetusbiggereetus@gmail.com>
2025-10-18 04:05:11 +0200

    Update NEO_EXT_CODES.lua

--- NEO_EXT_CODES.lua
+++ NEO_EXT_CODES.lua
@@ -28,7 +28,7 @@ return {
 	},
   	{
 		["Name"] = "InviteEvent",
-		['Limit'] = 50,
+		['Limit'] = false,
 		['Date'] = 0,
 		["Function"] = function(self)
 			if self.badges[3] then
@@ -37,6 +37,42 @@ return {
 				self:addBagItems{id = 'everstone', quantity = 1}
 				self:addBagItems{id = 'amuletcoin', quantity = 1}
 				return "Code Successfully Redeemed!"
+			else
+				return "You must have the 3rd Badge before redeeming this code.", true
+			end
+		end
+	},
+  	{
+		["Name"] = "AnthianRelease",
+		['Limit'] = false,
+		['Date'] = 0,
+		["Function"] = function(self)
+			if self.badges[3] then
+				self:addMoney(15000)
+				self:addBagItems{id = 'umvbattery', quantity = 3}
+				self:addBagItems{id = 'bottlecap', quantity = 1}
+				return "Code Successfully Redeemed!"
+			else
+				return "You must have the 3rd Badge before redeeming this code.", true
+			end
+		end
+	},
+  	{
+		["Name"] = "NewMegas",
+		['Limit'] = 50,
+		['Date'] = 0,
+		["Function"] = function(self)
+			if self.badges[4] then
+				i = math.random(1,3)
+				if i == 1 then
+				    id = 'delphoxite'
+				elseif i == 2 then
+				    id = 'delphoxite'
+				elseif i == 3 then
+				    id = 'chesnaughtite'
+				end
+				self:addBagItems{id = id, quantity = 1}
+    			return "Code Successfully Redeemed! You have obtained: " .. id
 			else
 				return "You must have the 3rd Badge before redeeming this code.", true
 			end
