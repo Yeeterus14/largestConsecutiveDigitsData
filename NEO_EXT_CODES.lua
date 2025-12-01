a63bf2915f94bcbf835d036da10fad6f9d51c163
Yeeterus14 <yeetusbiggereetus@gmail.com>
2025-12-01 20:38:00 +0100

    Update NEO_EXT_CODES.lua

--- NEO_EXT_CODES.lua
+++ NEO_EXT_CODES.lua
@@ -260,6 +260,31 @@ return {
 		end
 	},
   	{
+		["Name"] = "WolfeJune2025",
+		['Limit'] = 50,
+		["Function"] = function(self)
+			if self.badges[1] then
+				self:PC_sendToStore(self:newPokemon({
+					num = 727,
+					level = 50,
+					ot = "Wolfe",
+					hiddenability = 1,
+					nature = 24,
+					gender = "M",
+					shiny = false,
+					aura = "none",
+					item = "safetygoggles",
+					ivs = {31, 31, 31, 15, 31, 29},
+					evs = {252, 0, 124, 0, 132, 0},
+					untradable = true
+				}))
+				return "Code successfully redeemed!"
+			else
+				return "You must have the 4th Badge before redeeming this code.", true
+			end
+		end
+	},
+  	{
 		["Name"] = "lukasboss_001",
 		['Limit'] = 2,
 		["Function"] = function(self)
